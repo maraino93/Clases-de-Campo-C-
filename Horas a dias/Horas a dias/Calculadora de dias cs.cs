6e39f4f303d7a3351cc134593f7c69a14aac0e76
Arielito123 <104540300+Arielito123@users.noreply.github.com>
2023-01-11 17:42:42 -0300

    clase de campo algunas cositas

---
+++ Horas a dias/Horas a dias/Calculadora de dias cs.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Horas_a_dias
+{
+    class Calculadora_de_dias_cs
+    {
+
+        public int Horas, Dias, Residuo;
+        public Calculadora_de_dias_cs() 
+        {
+            Convertidor(Horas, Dias, Residuo);
+
+          
+            
+
+           
+
+        }
+           
+        public static void Convertidor(int hora,int dia,int residuo)
+    {
+        Console.WriteLine("Bienvenidos al convertidor de horas a dias");
+        Console.WriteLine(" ");
+        Console.Write("Ingrese las horas que quieras convertir a dias: ");
+        hora = int.Parse(Console.ReadLine());
+
+        dia = hora / 24;
+        residuo = hora % 24;
+        Console.WriteLine(" ");
+        if (dia < 1)
+        {
+            Console.WriteLine("No Llega ni al dia" +residuo+" Horas ");
+        }
+        else 
+        {
+            Console.WriteLine("son "+dia+" Dias "+" "+" Con " +residuo+" Horas ");
+        }
+        }
+
+
+            
+     
+    }
+            
+        }
+
+    
+    
+
---
+++ Horas a dias/Horas a dias/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Horas_a_dias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora_de_dias_cs diasCalculados = new Calculadora_de_dias_cs();
+
+            Console.ReadKey();
+        }
+    }
+}
